83e5c3274ae2a0e93c06ce4dc529e8a57703a11d
Marcio Camollez <marciocamollez@hotmail.com>
2022-12-26 15:49:05 -0300

    Envio

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
+import "./global.css";
+import styles from "./App.module.css";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/5040783?v=4",
+      name: "Marcio",
+      role: "WebDeveloper",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋 " },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "#developer" },
+    ],
+    publishedAt: new Date("2022-12-21 15:06:00"),
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
